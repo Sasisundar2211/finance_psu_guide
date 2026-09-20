e684eb1b8b2864d17db11ca0122115e658557746
Sasisundar2211 <sasisundar0926@gmail.com>
2026-09-20 11:22:16 +0530

    feat: implement public site

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", views.home, name="home"),
+    path("courses/", views.course_list, name="course_list"),
+    path("courses/<int:pk>/", views.course_detail, name="course_detail"),
+    path("mock-tests/", views.mock_test_list, name="mock_test_list"),
+    path("mock-tests/<int:pk>/", views.mock_test_detail, name="mock_test_detail"),
+    path("blog/", views.blog_list, name="blog_list"),
+    path("blog/<slug:slug>/", views.blog_detail, name="blog_detail"),
 ]
